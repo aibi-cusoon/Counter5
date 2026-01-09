a6234b0c681e781ee3ef49ffc47c446d18aad26e
aibi-cusoon <aibike.usenbaeva.ua@gmail.com>
2026-01-10 02:03:05 +0600

    homework2

--- app/src/main/java/com/geeks/counter5/presentation/count/CountActivity.kt
+++ app/src/main/java/com/geeks/counter5/presentation/count/CountActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.geeks.counter5.databinding.ActivityCountBinding
+import com.geeks.counter5.domain.model.TypeOfOperations
 import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class CountActivity : AppCompatActivity() {
@@ -33,6 +34,19 @@ class CountActivity : AppCompatActivity() {
 
         viewModel.countData.observe(this) { count ->
             binding.tvCounter.text = count.count.toString()
+
+            binding.tvOperation.text = when (count.typeOfOperation) {
+                TypeOfOperations.INCREMENT -> "Последняя операция: +"
+                TypeOfOperations.DECREMENT -> "Последняя операция: -"
+                TypeOfOperations.RESET -> "Последняя операция: reset"
+                TypeOfOperations.NONE -> " "
+
+            }
+            binding.tvIncrementCount.text =
+                "+ : ${count.countOfIncrement}"
+
+            binding.tvDecrementCount.text =
+                "- : ${count.countOfDecrement}"
         }
     }
 }
